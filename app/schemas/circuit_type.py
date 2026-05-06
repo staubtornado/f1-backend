44b94d96b93a19d0cabc9f576f83179cc484b083
Julian Benedikt Kirchner <jbkirche@gmail.com>
2026-05-06 20:10:02 +0200

    Fix wrong enum name

--- app/schemas/circuit_type.py
+++ app/schemas/circuit_type.py
@@ -2,5 +2,6 @@ from enum import Enum
 
 
 class CircuitType(str, Enum):
-    STREET = "street"
+    TEMPORARY_STREET = "temporary_street"
+    TEMPORARY_ROAD = "temporary_track"
     PERMANENT = "permanent"
